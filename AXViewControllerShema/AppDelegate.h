48c82a3498a85be2407b974f6f537dd4ebbdad63
devedbox <862099730@qq.com>
2016-10-11 12:24:18 +0800

    Initial Commit

---
+++ AXViewControllerShema/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXViewControllerShema
+//
+//  Created by devedbox on 2016/10/11.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
